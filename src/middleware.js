89193a226e056d1edeed72290881389123440b16
Naim Uddin <naimxofficial.bd@gmail.com>
2026-06-23 14:15:33 +0600

    added middleware

---
+++ src/middleware.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+
+export async function middleware(request) {
+  const session = getSessionCookie(request);
+  const { pathname } = request.nextUrl;
+
+  if (!session) {
+    const loginUrl = new URL("/login", request.url);
+    loginUrl.searchParams.set("callbackUrl", pathname);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
